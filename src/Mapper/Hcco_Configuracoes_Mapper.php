06157dab9042fcec44cbfccd5b122b8c46ebe802
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-04-14 12:14:55 -0300

    remove table name variable

--- src/Mapper/Hcco_Configuracoes_Mapper.php
+++ src/Mapper/Hcco_Configuracoes_Mapper.php
@@ -5,11 +5,6 @@ namespace Holos\Hcco\Mapper;
 class Hcco_Configuracoes_Mapper {
 
     /**
-     * Table name in the database
-     */
-    private static $table_name = 'hcco_configuracoes';
-
-    /**
      * Get an object
      *
      *
