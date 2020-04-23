26911ccc4634babd58201faaf313d43d0ee4c05b
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-04-22 22:16:52 -0300

    Changed checkout payment layout

--- resources/public/templates/hcco-checkout.php
+++ resources/public/templates/hcco-checkout.php
@@ -97,95 +97,103 @@
                     ?>
 
                     <div class="d-flex justify-content-center">
-                        <div class="payment-methods mb-5" id="paymentMethods">
+                        <div class="hcco-payment-section">
+                            <!-- Payment Tabs -->
+                            <div class="hcco-payment-tabs">
+                                <ul class="nav nav-pills" id="hcco-payment-tabs-list" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="hcco-payment-cc-tab" data-toggle="pill" href="#hcco-payment-cc" role="tab" aria-controls="hcco-payment-cc" aria-selected="true">
+                                            <i class="far fa-credit-card"></i> <span>Cartão de crédito</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="hcco-payment-picpay-tab" data-toggle="pill" href="#hcco-payment-picpay" role="tab" aria-controls="hcco-payment-picpay" aria-selected="false">
+                                            <i class="fab fa-paypal"></i> <span>PicPay</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- Payment Tabs -->
 
-                            <!-- Cartão de Credito -->
-                            <div class="payment-item">
-                                <div class="payment-item-header" id="paymentItemHeaderCreditCard">
-                                    <button type="button" class="btn btn-link payment-item-button" data-toggle="collapse" data-target="#paymentItemCreditCard" aria-expanded="true" aria-controls="paymentItemCreditCard">
-                                        Cartão de crédito
-                                        <img src="https://yevgenysim.github.io/shopper/assets/img/brands/color/cards.svg" alt="" class="ml-2">
-                                    </button>
-                                </div>
-                                <div id="paymentItemCreditCard" class="collapse show" aria-labelledby="paymentItemHeaderCreditCard" data-parent="#paymentMethods">
-                                    <div class="payment-item-content">
-                                        <!-- MP Script -->
-                                        <script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
-                                        <img src="https://imgmp.mlstatic.com/org-img/MLB/MP/BANNERS/tipo2_468X60.jpg?v=1" 
-                                        alt="Mercado Pago - Meios de pagamento" title="Mercado Pago - Meios de pagamento" 
-                                        class="img-fluid w-100 mb-3" />
-                                        
-                                        <form action="" method="POST" id="creditCardPaymentForm">
-                                            <?php wp_nonce_field( 'pagar_mercado_pago', 'pagar_mercado_pago_nonce' ); ?>
-                                            <div class="form-group">
-                                                <label for="cardNumber">Numero do cartão</label>
-                                                <!-- <input type="text" id="cardNumber" name="cardNumber" data-checkout="cardNumber" class="form-control" placeholder="0000 0000 0000 0000" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off> -->
-                                                <div class="input-group">
-                                                    <input type="text" id="cardNumber" name="cardNumber" data-checkout="cardNumber" class="form-control" placeholder="0000 0000 0000 0000">
-                                                    <span class="input-group-append">
-                                                        <img src="/wp-content/plugins/hcco/resources/public/img/credit-card.svg" id="creditCardPaymentInputIcon">
-                                                    </span>
+                            <!-- Payment Methods -->
+                            <div class="hcco-payment-methods">
+                                <div class="tab-content" id="pills-tabContent">
+
+                                    <!-- Crédit Card -->
+                                    <div class="tab-pane fade show active" id="hcco-payment-cc" role="tabpanel" aria-labelledby="hcco-payment-cc-tab">
+                                        <div class="hcco-payment-method-content">
+                                            <!-- MP Script -->
+                                            <script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
+                                            <img src="https://imgmp.mlstatic.com/org-img/MLB/MP/BANNERS/tipo2_468X60.jpg?v=1" 
+                                            alt="Mercado Pago - Meios de pagamento" title="Mercado Pago - Meios de pagamento" 
+                                            class="img-fluid w-100 mb-3" />
+                                            
+                                            <form action="" method="POST" id="creditCardPaymentForm">
+                                                <?php wp_nonce_field( 'pagar_mercado_pago', 'pagar_mercado_pago_nonce' ); ?>
+                                                <div class="form-group">
+                                                    <label for="cardNumber">Numero do cartão</label>
+                                                    <!-- <input type="text" id="cardNumber" name="cardNumber" data-checkout="cardNumber" class="form-control" placeholder="0000 0000 0000 0000" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off> -->
+                                                    <div class="input-group">
+                                                        <input type="text" id="cardNumber" name="cardNumber" data-checkout="cardNumber" class="form-control" placeholder="0000 0000 0000 0000">
+                                                        <span class="input-group-append">
+                                                            <img src="/wp-content/plugins/hcco/resources/public/img/credit-card.svg" id="creditCardPaymentInputIcon">
+                                                        </span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="cardholderName">Nome no cartão</label>
-                                                <input type="text" id="cardholderName" name="cardholderName" data-checkout="cardholderName" class="form-control">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="docNumber">CPF</label>
-                                                <input type="text" id="docNumber" name="docNumber" data-checkout="docNumber" class="form-control" placeholder="000 000 000 00" >
-                                                <input type="hidden" id="docType" data-checkout="docType" value="CPF">
-                                            </div>
-                                            <div class="form-row">
-                                                <div class="col-sm-6">
-                                                    <div class="form-group">
-                                                        <label for="expirationMonth">Data de validade</label>
-                                                        <div class="input-group">
-                                                            <input type="text" id="cardExpirationMonth" name="cardExpirationMonth" data-checkout="cardExpirationMonth" class="form-control" placeholder="MM" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off>
-                                                            <input type="text" id="cardExpirationYear" name="cardExpirationYear" data-checkout="cardExpirationYear" class="form-control" placeholder="AAAA" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off>
+                                                <div class="form-group">
+                                                    <label for="cardholderName">Nome no cartão</label>
+                                                    <input type="text" id="cardholderName" name="cardholderName" data-checkout="cardholderName" class="form-control">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="docNumber">CPF</label>
+                                                    <input type="text" id="docNumber" name="docNumber" data-checkout="docNumber" class="form-control" placeholder="000 000 000 00" >
+                                                    <input type="hidden" id="docType" data-checkout="docType" value="CPF">
+                                                </div>
+                                                <div class="form-row">
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <label for="expirationMonth">Data de validade</label>
+                                                            <div class="input-group">
+                                                                <input type="text" id="cardExpirationMonth" name="cardExpirationMonth" data-checkout="cardExpirationMonth" class="form-control" placeholder="MM" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off>
+                                                                <input type="text" id="cardExpirationYear" name="cardExpirationYear" data-checkout="cardExpirationYear" class="form-control" placeholder="AAAA" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <label for="securityCode">Código CVC</label>
+                                                            <div class="input-group">
+                                                                <input type="text" id="securityCode" name="securityCode" data-checkout="securityCode" class="form-control" placeholder="CVC" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off>
+                                                                <span class="input-group-append">
+                                                                    <img src="/wp-content/plugins/hcco/resources/public/img/security-code.svg" id="creditCardPaymentInputIcon">
+                                                                </span>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="col-sm-6">
-                                                    <div class="form-group">
-                                                        <label for="securityCode">Código CVC</label>
-                                                        <div class="input-group">
-                                                            <input type="text" id="securityCode" name="securityCode" data-checkout="securityCode" class="form-control" placeholder="CVC" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off>
-                                                            <span class="input-group-append">
-                                                                <img src="/wp-content/plugins/hcco/resources/public/img/security-code.svg" id="creditCardPaymentInputIcon">
-                                                            </span>
-                                                        </div>
-                                                    </div>
+
+                                                <input type="hidden" name="amount" id="amount" value="<?php echo $pedido->get_preco(); ?>" />
+                                                <input type="hidden" name="paymentMethodId" />
+
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary d-block w-100"><i class="fas fa-lock"></i> Pagar R$ <?php echo $pedido->get_preco(); ?></button>
                                                 </div>
-                                            </div>
-
-                                            <input type="hidden" name="amount" id="amount" value="<?php echo $pedido->get_preco(); ?>" />
-                                            <input type="hidden" name="paymentMethodId" />
-
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-primary d-block w-100"><i class="fas fa-lock"></i> Pagar R$ <?php echo $pedido->get_preco(); ?></button>
-                                            </div>
-                                        </form>
+                                            </form>
+                                        </div>
                                     </div>
+                                    <!-- Crédit Card -->
+
+                                    <!-- PicPay -->
+                                    <div class="tab-pane fade" id="hcco-payment-picpay" role="tabpanel" aria-labelledby="hcco-payment-picpay-tab">
+                                        <div class="hcco-payment-method-content">
+                                            <p>PicPay</p>
+                                        </div>
+                                    </div>
+                                    <!-- PicPay -->
+
                                 </div>
                             </div>
-                            <!-- Cartão de Crédito -->
-
-                            <!-- Picpay -->
-                            <div class="payment-item">
-                                <div class="payment-item-header" id="paymentItemHeaderPicpay">
-                                    <h5>
-                                        <button type="button" class="btn btn-link payment-item-button collapsed" data-toggle="collapse" data-target="#paymentItemPicpay" aria-expanded="false" aria-controls="paymentItemPicpay">Picpay</button>
-                                    </h5>
-                                </div>
-                                <div id="paymentItemPicpay" class="collapse" aria-labelledby="paymentItemHeaderPicpay" data-parent="#paymentMethods">
-                                    <div class="payment-item-content">
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, atque.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Picpay -->
-
+                            <!-- Payment Methods -->
                         </div>
                     </div>
                 </div>
