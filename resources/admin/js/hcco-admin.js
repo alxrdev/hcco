8c71209fb968837203abab9bde76179be43dfbf9
alxrdev <rodriguesalex793@gmail.com>
2020-04-28 17:51:01 -0300

    Implementing delete curriculo

--- resources/admin/js/hcco-admin.js
+++ resources/admin/js/hcco-admin.js
@@ -3,11 +3,12 @@
 const hcco_delete_curriculo = (nonce, id) => {
     httpRequest = new XMLHttpRequest()
     httpRequest.onreadystatechange = () => {
-       if ( httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200 ) {
-           console.log( 'works' )
-       }
+        if ( httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200 ) {
+            console.log( 'works' )
+        }
     }
 
-    httpRequest.open('GET', `${hcco_ajax_object.ajax_url}?_wpnonce=${nonce}&curriculo_id=${id}&action=${hcco_ajax_object.delete_curriculo_action}`)
-    httpRequest.send()
+    httpRequest.open('POST', hcco_ajax_object.ajax_url)
+    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    httpRequest.send(`_wpnonce=${nonce}&curriculo_id=${id}&action=${hcco_ajax_object.delete_curriculo_action}`)
 }
