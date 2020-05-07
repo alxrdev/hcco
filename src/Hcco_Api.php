09711d14efa7f48474d16f99cf324f6d7d70f2c6
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-05-07 09:43:27 -0300

    Register the picpay api controller

--- src/Hcco_Api.php
+++ src/Hcco_Api.php
@@ -8,29 +8,31 @@ use Holos\Hcco\Api\Hcco_PicPay_Controller;
 class Hcco_Api {
 
     /**
-	 * Propertie that stores the plugin name.
-	 * 
-	 * @since 	1.0.0
-	 * @access 	private
-	 */
+     * Propertie that stores the plugin name.
+     * 
+     * @since 	1.0.0
+     * @access 	private
+     * @var     string      The plugin name.
+     */
     private $plugin_name;
     
     /**
-	 * Propertie that stores the plugin version.
-	 * 
-	 * @since 	1.0.0
-	 * @access 	private
-	 */ 
+     * Propertie that stores the plugin version.
+     * 
+     * @since 	1.0.0
+     * @access 	private
+     * @var     string      The plugin version.
+     */ 
     private $version;
 
     /**
-	 * The class constructor.
-	 * 
-	 * @since 	1.0.0
-	 * @access 	public
-	 * @param 	string	$plugin_name The plugin name.
-	 * @param	string 	$version The plugin version.
-	 */
+     * The class constructor.
+     * 
+     * @since 	1.0.0
+     * @access 	public
+     * @param 	string	$plugin_name The plugin name.
+     * @param	string 	$version The plugin version.
+     */
     public function __construct( $plugin_name, $version ) {
 
         $this->plugin_name = $plugin_name;
