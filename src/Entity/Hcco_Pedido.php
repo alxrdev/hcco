079714fe1433032a44f074609a38a6b9dedd73bb
alxrdev <rodriguesalex793@gmail.com>
2020-05-07 14:09:53 -0300

    Turned public gerar_codigo_referencia method

--- src/Entity/Hcco_Pedido.php
+++ src/Entity/Hcco_Pedido.php
@@ -6,24 +6,95 @@ use Holos\Hcco\Entity\Hcco_Entity;
 
 class Hcco_Pedido extends Hcco_Entity {
 
-    /*
-     * Class properties
+    /**
+     * Propertie that stores the pedido id.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
      */
     private $id = '';
+
+    /**
+     * Propertie that stores the curriculo id.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $curriculo_id  = '';
+
+    /**
+     * Propertie that stores the usuario id.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $usuario_id  = '';
+
+    /**
+     * Propertie that stores the pedido's codigo de referencia.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $codigo_referencia = '';
+
+    /**
+     * Propertie that stores the pedido's payment id.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $payment_id = '';
+
+    /**
+     * Propertie that stores the preco.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $preco = '';
+
+    /**
+     * Propertie that stores the status do pagamento.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $status_pagamento = '';
+
+    /**
+     * Propertie that stores the pedido's created date.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $criado_em = '';
+
+
+    /**
+     * Propertie that stores the pedido's updated date.
+     * 
+     * @since   1.0.0
+     * @access  private
+     * @var     string 
+     */
     private $atualizado_em = '';
 
     /**
-     * Método que gera o código de referencia do pedido
-     * utilizando o nome do cliente e datetime no método md5
+     * Method that create a reference code from pedido.
+     * 
+     * @since   1.0.0
+     * @access  public
      */
-    public function gerar_codigo_referencia() {
+    public function gerar_codigo_referencia() : void {
 
         $this->set_codigo_referencia( 'HOLOS_' . md5( current_time( 'd/m/yy h:m:s' ) . rand( 0, 100 ) ) );
 
@@ -35,109 +106,235 @@ class Hcco_Pedido extends Hcco_Entity {
     |--------------------------------------------------------------------------
     */
 
-    public function get_id() {
+    /**
+     * Method that returns the pedido id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string      The pedido id.
+     */
+    public function get_id() : string {
 
         return $this->id;
     
     }
 
-    public function set_id( $id ) {
+    /**
+     * Method that set the pedido id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   int|string      $id The pedido id.
+     */
+    public function set_id( $id ) : void {
 
         $this->id = $id;
     
     }
 
-    public function get_curriculo_id()  {
+    /**
+     * Method that returns the curriculo id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string      The curriculo id.
+     */
+    public function get_curriculo_id() : string  {
 
         return $this->curriculo_id;
     
     }
 
-    public function set_curriculo_id( $curriculo_id ) {
+    /**
+     * Method that set the curriculo id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   int|string      $curriculo_id The curriculo id.
+     */
+    public function set_curriculo_id( $curriculo_id ) : void {
 
         $this->curriculo_id = $curriculo_id;
     
     }
 
-    public function get_usuario_id() {
+    /**
+     * Method that returns the usuario id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string         The usuario id.
+     */
+    public function get_usuario_id() : string {
 
         return $this->usuario_id;
     
     }
 
-    public function set_usuario_id( $usuario_id ) {
+    /**
+     * Method that set the usuario id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   int|string      $usuario_id The usuario id.
+     */
+    public function set_usuario_id( $usuario_id ) : void {
 
         $this->usuario_id = $usuario_id;
     
     }
 
-    public function get_codigo_referencia() {
+    /**
+     * Method that returns the codigo de referencia.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string      The codigo de referencia.
+     */
+    public function get_codigo_referencia() : string {
 
         return $this->codigo_referencia;
     
     }
 
-    protected function set_codigo_referencia( $codigo_referencia ) {
+    /**
+     * Method that set the codigo de referencia.
+     * 
+     * @since   1.0.0
+     * @access  protected
+     * @param   string      $codigo_referencia The codigo de referencia.
+     */
+    protected function set_codigo_referencia( string $codigo_referencia ) : void {
 
         $this->codigo_referencia = $codigo_referencia;
     
     }
 
-    public function get_payment_id() {
+    /**
+     * Method that returns the payment id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string        The payment id.
+     */
+    public function get_payment_id() : string {
 
         return $this->payment_id;
     
     }
 
-    public function set_payment_id( $payment_id ) {
+    /**
+     * Method that set the payment id.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   string      $payment_id The payment id.
+     */
+    public function set_payment_id( string $payment_id ) : void {
 
         $this->payment_id = $payment_id;
     
     }
 
+    /**
+     * Method that returns the preco.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string      The preco.
+     */
     public function get_preco() {
 
         return $this->preco;
     
     }
 
-    public function set_preco( $preco ) {
+    /**
+     * Method that set the preco.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   int|string|float      $preco The preco.
+     */
+    public function set_preco( $preco ) : void {
 
         $this->preco = $preco;
     
     }
 
-    public function get_status_pagamento() {
+    /**
+     * Method that returns the payment status.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string      The payment status.
+     */
+    public function get_status_pagamento() : string {
 
         return $this->status_pagamento;
     
     }
 
-    public function set_status_pagamento( $status_pagamento ) {
+    /**
+     * Method that set the status de pagamento.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   string      $status_pagamento The status de pagamento.
+     */
+    public function set_status_pagamento( string $status_pagamento ) : void {
 
         $this->status_pagamento = $status_pagamento;
     
     }
 
-    public function get_criado_em() {
+    /**
+     * Method that returns the pedido's created at date.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string    The created at date.
+     */
+    public function get_criado_em() : string {
 
         return $this->criado_em;
     
     }
 
-    public function set_criado_em( $criado_em ) {
+    /**
+     * Method that set the pedido's created at.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   string      $criado_em The pedido's created at.
+     */
+    public function set_criado_em( $criado_em ) : void {
 
         $this->criado_em = $criado_em;
     
     }
 
-    public function get_atualizado_em() {
+    /**
+     * Method that returns the pedido's updated at date.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @return  string      The pedido's updated at date.
+     */
+    public function get_atualizado_em() : string {
 
         return $this->atualizado_em;
     
     }
 
-    public function set_atualizado_em( $atualizado_em ) {
+    /**
+     * Method that set the pedido's updated at.
+     * 
+     * @since   1.0.0
+     * @access  public
+     * @param   string      $criado_em The pedido's updated at.
+     */
+    public function set_atualizado_em( $atualizado_em ) : void {
 
         $this->atualizado_em = $atualizado_em;
     
