c08346876147dbc0d87cc4a4d59452d897862eba
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-05-15 11:12:18 -0300

    Created a menu interface

---
+++ src/Admin/Menu/Hcco_Menu_Interface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Holos\Hcco\Admin\Menu;
+
+interface Hcco_Menu_Interface {
+
+    /**
+     * The initial point of the menu.
+     * 
+     * @since   1.0.0
+     * @access  public
+     */
+    public function index();
+
+}
